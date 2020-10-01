4d9fbbf6fa4ef4a9386d2b068c2167608d045898
MnMaps <mnmaps.nigro.m@gmail.com>
2020-10-01 15:01:23 +0200

    1ott5thCommit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 console.log('app-started')
 if('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('https://mnmaps.github.io/testPWA/testPWA/sw.js')
+    navigator.serviceWorker.register('sw.js')
         .then((reg) => console.log('service worker registered', reg))
         .catch((err) => console.log('service worker not registered', err));
 }
